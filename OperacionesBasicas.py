7b74ee68ce769162a901ea5c4ddd737ba5b2240a
Pas99-to <71859268+Pas99-to@users.noreply.github.com>
2022-10-05 07:28:10 -0500

    Add files via upload

---
+++ OperacionesBasicas.py
@@ -0,0 +1,15 @@
+
+#sumar, restar, multiplicar y dividir.
+
+def sumar(a:int , b:int):
+    return  a + b
+
+
+def multiplicar(a:int,b:int):
+    return a * b
+
+
+def dividir(a:int,b:int):
+    return a/b
+
+
---
+++ main_oper.py
@@ -0,0 +1,11 @@
+import OperacionesBasicas as Oper
+
+def calculadora():
+    
+    print("La suma de 6 + 8 es: ",Oper.sumar(6,8))
+    print("La multiplicación de 11 * 8 es: ",Oper.multiplicar(11,8))
+    print("La división de 10 / 8 es: ",Oper.dividir(10,8))
+
+calculadora()
+    
+    

bc8e4ad8d5a1cbcd24ef32d1ea743822c7b6279f
Pas99-to <71859268+Pas99-to@users.noreply.github.com>
2022-10-05 07:22:38 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import pprint
+#import operaciones #as o
+import sys
+
+def main():
+
+    #res = o.suma(2,2)
+
+    #resREsta = o.resta(5, 3)
+
+    #op = operaciones.Operador() 
+
+    #print("HOla en main()",res, resREsta)
+
+    #print(op.multiplicacion(4,2))
+
+    #print(operaciones.PI)
+
+    #print(sys.path)
+
+    pprint.pprint(sys.path)
+
+if __name__ == '__main__':
+    main()
